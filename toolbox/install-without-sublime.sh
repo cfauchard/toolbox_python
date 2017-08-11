579a5e30f9fa56f43088d3ed206d99679cb52eeb
griou <ext.goulven.riou@sncf.fr>
2017-08-11 12:10:35 +0200

    add fbdtools install script add script for installation without sublime

--- toolbox/install-without-sublime.sh
+++ toolbox/install-without-sublime.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-
-./install-sublime.sh &&
 echo "installing sphinx-doc"; 
 sudo apt-get install sphinx-doc &&
 echo "sphinx-doc successfully installed"; 
@@ -14,5 +12,7 @@ cd ../ && rm -rf .git && cd projectid && git init &&
 echo "git initied successfully" &&
 echo "you can link your git repo with \'git remote add origin [git url]\'";
 echo "install fbdtools";
-cd /tmp && git clone -b dev git@gitlab.com:sncf_fbd/fbd_tools_project.git && cd /tmp/fbd_tools_project && python setup.py install &&
+cd /tmp && git clone -b dev git@gitlab.com:sncf_fbd/fbd_tools_project.git && 
+cd /tmp/fbd_tools_project && python setup.py install &&
+rm -rf /tmp/fbd_tools_project
 echo "fbdtools successfully installed"
