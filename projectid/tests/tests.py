6f89c94e4bd77cdbe53ca889b47176aa28f2dfa4
gintrona <guido@intronati.com.ar>
2017-08-22 16:32:34 +0200

    FIX test unitaire function du module utils teste ; sys.path modifie

--- projectid/tests/tests.py
+++ projectid/tests/tests.py
@@ -1,12 +1,25 @@
-def test_len_must_succes():
-	assert len("hello") == 5
+import sys
+import os
 
-def test_len_must_fail():
-	assert len("hello") not 5
+sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../modules")
+
+import utils.aux
+
+
+def test_len_must_succes():
+    assert len("hello") == 5
+
 
 def test_string_content_must_success():
-	teststr = "hello"
-	assert teststr == "hello"
+    teststr = "hello"
+    assert teststr == "hello"
+
 
 def test_stringequal():
-	assert len("hello") == 5
+    assert len("hello") == 5
+
+
+def test_count_words():
+    resu = utils.aux.count_words('aabbcde')
+    dico_ref = {'b': 2, 'a': 2, 'c': 1, 'e': 1, 'd': 1}
+    assert resu, dico_ref
