38a95c5e20d04333c78dd5b3c0b99a159c33a880
gintrona <guido@intronati.com.ar>
2017-08-22 12:22:56 +0200

    FIX choose fbd-tools' branch to install

--- toolbox/install-fbd_tools.sh
+++ toolbox/install-fbd_tools.sh
@@ -1,4 +1,5 @@
 echo 'Creating temporary directory:'; echo $TOOL_BOX_ROOT/tmp;
 mkdir $TOOL_BOX_ROOT/tmp;
-cd $TOOL_BOX_ROOT/tmp && git clone -b dev git@gitlab.com:sncf_fbd/fbd_tools_project.git ;
+read -p "Choose the fbd_tools' branch to install [develop (default), release, master]: " branch;
+cd $TOOL_BOX_ROOT/tmp && git clone -b $branch git@gitlab.com:sncf_fbd/fbd_tools_project.git ;
 cd $TOOL_BOX_ROOT/tmp/fbd_tools_project/ && python3 setup.py install
