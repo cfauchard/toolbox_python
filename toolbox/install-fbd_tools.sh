bdcbf587dfb5d68db86442fde431f8bdabc89f86
guido <guido.intronati@sncf.fr>
2017-08-23 08:27:45 +0000

    Update install-fbd_tools.sh (default branch to clone)

--- toolbox/install-fbd_tools.sh
+++ toolbox/install-fbd_tools.sh
@@ -1,5 +1,8 @@
 echo 'Creating temporary directory:'; echo $TOOL_BOX_ROOT/tmp;
 mkdir $TOOL_BOX_ROOT/tmp;
 read -p "Choose the fbd_tools' branch to install [develop (default), release, master]: " branch;
+if test "$branch" = "" 
+then branch="develop"
+fi
 cd $TOOL_BOX_ROOT/tmp && git clone -b $branch git@gitlab.com:sncf_fbd/fbd_tools_project.git ;
 cd $TOOL_BOX_ROOT/tmp/fbd_tools_project/ && python3 setup.py install
